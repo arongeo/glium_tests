6698339879271c250800dc67dad460a1d700f67a
arongeo <me@arongeo.com>
2023-02-23 23:16:38 +0100

    Drew a triangle

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ extern crate glium;
 
 use glium::glutin;
 use glium::Surface;
+use glium::implement_vertex;
 
 #[derive(Copy, Clone)]
 struct Vertex {
-    posititon: [f32; 2],
-};
+    position: [f32; 2],
+}
 
 impl Vertex {
     pub fn new(x_pos: f32, y_pos: f32) -> Self {
@@ -34,10 +35,33 @@ fn main() {
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(0.0, 1.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
     event_loop.run(move |event, _, control_flow| {
 
         let mut target = display.draw();
         target.clear_color(0.5, 0.8, 1.0, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
         target.finish().unwrap();
 
         let next_frame_time = std::time::Instant::now() + 
