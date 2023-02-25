056cbbcc516ed32dee1b0d4cfa06bcc9923d3cc8
arongeo <me@arongeo.com>
2023-02-25 14:54:50 +0100

    Started working on textures

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 
 extern crate glium;
+extern crate image;
 
 use glium::glutin;
 use glium::Surface;
 use glium::implement_vertex;
 use glium::uniform;
 
+use std::io::Cursor;
+
 #[derive(Copy, Clone)]
 struct Vertex {
     position: [f32; 2],
+    tex_coordinates: [f32; 2],
 }
 
 impl Vertex {
-    pub fn new(x_pos: f32, y_pos: f32) -> Self {
+    pub fn new(x_pos: f32, y_pos: f32, x_tex_coord: f32, y_tex_coord: f32) -> Self {
         Vertex {
             position: [x_pos, y_pos],
+            tex_coordinates: [x_tex_coord, y_tex_coord],
         }
     }
 }
@@ -25,12 +30,17 @@ fn main() {
     let context_builder = glutin::ContextBuilder::new();
     let display = glium::Display::new(window_builder, context_builder, &event_loop).unwrap();
 
+    let image = image::load(Cursor::new(&include_bytes!("/home/arongeo/Downloads/obama.jpg")), image::ImageFormat::Jpeg).unwrap().to_rgba8();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
+
     implement_vertex!(Vertex, position);
 
 
-    let vertex1 = Vertex::new(-0.5, -0.5);
-    let vertex2 = Vertex::new(0.0, 0.5);
-    let vertex3 = Vertex::new(0.5, -0.25);
+    let vertex1 = Vertex::new(-0.5, -0.5, 0.0, 0.0);
+    let vertex2 = Vertex::new(0.0, 0.5, 0.0, 1.0);
+    let vertex3 = Vertex::new(0.5, -0.25, 1.0, 0.0);
 
     let shape = vec![vertex1, vertex2, vertex3];
 
@@ -41,10 +51,12 @@ fn main() {
         #version 140
 
         in vec2 position;
+        out vec2 my_attr;
 
         uniform mat4 matrix;
 
         void main() {
+            my_attr = position;
             gl_Position = matrix * vec4(position, 0.0, 1.0);
         }
     "#;
@@ -52,10 +64,11 @@ fn main() {
     let fragment_shader_src = r#"
         #version 140
 
+        in vec2 my_attr;
         out vec4 color;
 
         void main() {
-            color = vec4(0.0, 1.0, 0.0, 1.0);
+            color = vec4(my_attr, 0.0, 1.0);
         }
     "#;
 
